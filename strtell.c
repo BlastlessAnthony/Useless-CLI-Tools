bc5bd7af129adca9907cd3b98e6e9e51ca4677c3
BlastlessAnthony <blastlessanthony@hotmail.com>
2023-07-15 23:14:14 -0400

    Stuff.

---
+++ strtell.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+    for (int i = 1; i < argc; ++i) {
+        fprintf(stdout, "[%d]: \"%s\" Size: %lu", i, argv[i], strlen(argv[i]));
+    }
+    exit(EXIT_SUCCESS);
+}
